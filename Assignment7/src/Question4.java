34efa2d5b95c09efec8a54944e05ec5c9e34c9bc
Ramya Hebbar <hebbar.r@husky.neu.edu>
2019-11-04 23:39:50 -0800

    Assignment7 final commit

--- Assignment7/src/Question4.java
+++ Assignment7/src/Question4.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class Question4 {
-    private static Object ANSWER_LOCK = new Object();
+    private long finalSum = 0;
 
     /*generate array of random numbers*/
-    static void generateRandomArray(int[] arr){
+    static void generateRandomArray(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Util.getRandomNumber(100);
@@ -10,46 +12,32 @@ public class Question4 {
     }
 
     /*get sum of an array using 4 threads*/
-    static long sum(int[] arr){
+    static long sum(int[] arr) {
         int finalSum = 0;
-
-        String string = new String();
 
         for (int i = 0; i < arr.length; i++) {
 
-            if(i == 0) {
-                string = "Sum of " + arr[i];
-            }
-            else {
-                string = string + " + " + arr[i];
-            }
-
             finalSum += arr[i];
         }
-
-        string = string + " is " + finalSum;
-
-        System.out.println(string);
 
         return finalSum;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        int[] arr = new int[40000];
+        int[] arr = new int[4000000];
         generateRandomArray(arr);
 
         sumTheValuesInParallelForArray(arr);
-
-//        long sum = sum(arr);
-//        System.out.println("Sum: " + sum);
     }
 
-    private static void sumTheValuesInParallelForArray(int[] arr) throws InterruptedException{
+    private static void sumTheValuesInParallelForArray(int[] arr) throws InterruptedException {
 
-        SummingThread thread1 = new SummingThread(arr);
-        SummingThread thread2 = new SummingThread(arr);
-        SummingThread thread3 = new SummingThread(arr);
-        SummingThread thread4 = new SummingThread(arr);
+        int arraySplitter = arr.length / 4;
+
+        SummingThread thread1 = new SummingThread(arr, 0, arraySplitter);
+        SummingThread thread2 = new SummingThread(arr, arraySplitter + 1, arraySplitter * 2);
+        SummingThread thread3 = new SummingThread(arr, (arraySplitter * 2) + 1, arraySplitter * 3);
+        SummingThread thread4 = new SummingThread(arr, (arraySplitter * 3) + 1, arr.length);
 
         thread1.start();
         thread2.start();
@@ -64,23 +52,25 @@ public class Question4 {
 
     static class SummingThread extends Thread {
         private int[] arr;
+        private int startIndex;
+        private int endIndex;
 
-        public SummingThread(int[] arr) {
+        public SummingThread(int[] arr, int startIndex, int endIndex) {
             this.arr = arr;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
         }
 
         @Override
         public void run() {
             Question4 q4 = new Question4();
-            long sum = q4.sumTheValues(this.arr);
-            System.out.println("Sum is : " + sum);
+            q4.sumTheValues(Arrays.copyOfRange(this.arr, this.startIndex, this.endIndex));
         }
     }
 
-    public synchronized long sumTheValues(int[] arr) {
-        synchronized (ANSWER_LOCK) {
-            return sum(arr);
-        }
+    public synchronized void sumTheValues(int[] arr) {
+        finalSum += sum(arr);
+        System.out.println("Sum is : " + finalSum);
     }
 }
 
