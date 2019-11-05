1d650e0f8b0c5af4afe419d965b6e4c96229d85f
Ramya Hebbar <hebbar.r@husky.neu.edu>
2019-11-04 22:55:06 -0800

    Assignment7 second commit.

--- Assignment7/src/Question2.java
+++ Assignment7/src/Question2.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Question2 {
 
     public String decodeTheString(String encodedString) {
-        String decodedString = ";";
+        String decodedString = new String();
 
         char[] allChars = encodedString.toCharArray();
 
@@ -11,37 +12,78 @@ public class Question2 {
 
         char ch = allChars[i];
 
-        ArrayList<Character> charactersOfFinalString = new ArrayList<Character>();
+        Stack<String> charactersOfFinalString = new Stack<String>();
 
-        while (i < allChars.length) {
+        int n = 0;
 
-            int n = 0;
-
-            while(Character.isDigit(ch)) {
-                n = n*10 + Character.getNumericValue(ch);
+        while (i < allChars.length - 1) {
+            while (Character.isDigit(ch)) {
+                n = n * 10 + Character.getNumericValue(ch);
                 ch = allChars[++i];
             }
 
-            if(ch == '[' ) {
+            if(n == 0) {
+                n = 1;
+            }
+
+            if (ch == '[') {
                 ch = allChars[++i];
             }
 
-            while(Character.isLetter(ch)) {
-                charactersOfFinalString.add(ch);
-                ch = allChars[++i];
+            while (Character.isLetter(ch)) {
+
+                if (charactersOfFinalString.size() > 0) {
+                    String existingString = charactersOfFinalString.pop();
+                    existingString = existingString + Character.toString(ch);
+                    charactersOfFinalString.push(existingString);
+                } else {
+                    charactersOfFinalString.push(Character.toString(ch));
+                }
+
+                if (i + 1 < allChars.length) {
+                    ch = allChars[++i];
+                }
+                else {
+                    ch = ']';
+                }
             }
 
-            if(Character.isDigit(ch)) {
-                //add internal string
+            if (ch == ']') {
+                if (i + 1 < allChars.length) {
+                    ch = allChars[++i];
+                }
+
+                String stringToAppend = null;
+
+                if (charactersOfFinalString.size() > 0) {
+                    stringToAppend = charactersOfFinalString.pop();
+
+                    //construct string here
+                    for (int j = 0; j < n; j++) {
+                        decodedString = decodedString + stringToAppend;
+                    }
+
+                    n = 0;
+                }
+            } else {
+                //internal string - loop with existing values
+                String internalString = new String();
+
+                while (ch != ']') {
+                    internalString = internalString + Character.toString(ch);
+                    ch = allChars[++i];
+                }
+
+                internalString = internalString + Character.toString(ch);
+
+                String finalInternalString = decodeTheString(internalString);
+
+                if (charactersOfFinalString.size() > 0) {
+                    String existingString = charactersOfFinalString.pop();
+                    existingString = existingString + finalInternalString;
+                    charactersOfFinalString.push(existingString);
+                }
             }
-            else if(ch == ']') {
-                ch = allChars[++i];
-
-                //construct string here
-            }
-
-//            if(allChars[i+1] )
-
         }
 
         return decodedString;
@@ -49,5 +91,9 @@ public class Question2 {
 
     public static void main(String[] args) {
 
+        Question2 q2 = new Question2();
+        System.out.println("Encoded string 3[a]2[bc] is decoded to : " + q2.decodeTheString("3[a]2[bc]"));
+        System.out.println("Encoded string 3[a2[c]] is decoded to : " + q2.decodeTheString("3[a2[c]]"));
+        System.out.println("Encoded string 2[abc]3[cd]ef is decoded to : " + q2.decodeTheString("2[abc]3[cd]ef"));
     }
 }
--- Assignment7/src/Question4.java
+++ Assignment7/src/Question4.java
@@ -1,18 +1,93 @@
 public class Question4 {
+    private static Object ANSWER_LOCK = new Object();
+
     /*generate array of random numbers*/
     static void generateRandomArray(int[] arr){
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Util.getRandomNumber(100);
+        }
     }
 
     /*get sum of an array using 4 threads*/
     static long sum(int[] arr){
-        return 0;
+        int finalSum = 0;
+
+        String string = new String();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(i == 0) {
+                string = "Sum of " + arr[i];
+            }
+            else {
+                string = string + " + " + arr[i];
+            }
+
+            finalSum += arr[i];
+        }
+
+        string = string + " is " + finalSum;
+
+        System.out.println(string);
+
+        return finalSum;
     }
 
-    public static void main(String[] args){
-        int[] arr = new int[4000000];
-//        generateArray(arr);
+    public static void main(String[] args) throws InterruptedException {
+        int[] arr = new int[40000];
+        generateRandomArray(arr);
+
+        sumTheValuesInParallelForArray(arr);
+
 //        long sum = sum(arr);
 //        System.out.println("Sum: " + sum);
     }
+
+    private static void sumTheValuesInParallelForArray(int[] arr) throws InterruptedException{
+
+        SummingThread thread1 = new SummingThread(arr);
+        SummingThread thread2 = new SummingThread(arr);
+        SummingThread thread3 = new SummingThread(arr);
+        SummingThread thread4 = new SummingThread(arr);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+    }
+
+    static class SummingThread extends Thread {
+        private int[] arr;
+
+        public SummingThread(int[] arr) {
+            this.arr = arr;
+        }
+
+        @Override
+        public void run() {
+            Question4 q4 = new Question4();
+            long sum = q4.sumTheValues(this.arr);
+            System.out.println("Sum is : " + sum);
+        }
+    }
+
+    public synchronized long sumTheValues(int[] arr) {
+        synchronized (ANSWER_LOCK) {
+            return sum(arr);
+        }
+    }
 }
+
+class Util {
+    static int getRandomNumber(int x) {
+        return (int) (Math.random() * x);
+    }
+}
+
+
